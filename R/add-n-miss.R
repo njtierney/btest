a5cabe9c7514bead41c99a756b84ad8e29b3d122
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-01-27 20:56:05 -0800

    implement old method

--- R/add-n-miss.R
+++ R/add-n-miss.R
@@ -1,14 +1,25 @@
 add_n_miss <- function(data, ..., label = "n_miss"){
   if (missing(...)) {
-    data[[paste0(label, "_all")]] <- rowSums(is.na(data))
+    purrrlyr::by_row(.d = data,
+                     ..f = function(x) n_miss(x),
+                     .collate = "row",
+                     .to = paste0(label,"_all"))
   } else {
 
     quo_vars <- rlang::quos(...)
 
     selected_data <- dplyr::select(data, !!!quo_vars)
 
-    data[[paste0(label, "_vars")]] <- rowSums(is.na(selected_data))
+    prop_selected_data <- purrrlyr::by_row(.d = selected_data,
+                                           ..f = function(x) n_miss(x),
+                                           .collate = "row",
+                                           .to =  paste0(label,"_vars"))
+
+    # add only the variables prop_miss function, not the whole data.frame...
+    prop_selected_data_cut <- prop_selected_data %>%
+      dplyr::select(!!as.name(paste0(label,"_vars")))
+
+    dplyr::bind_cols(data, prop_selected_data_cut) %>% dplyr::as_tibble()
   } # close else loop
 
-  data
 }
